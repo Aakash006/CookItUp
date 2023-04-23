9b12b946d9e013d3492a1a6344308894d9238921
Aakash Patel <aakashpatel006@Aakashs-MacBook-Pro-2.local>
2023-04-23 13:11:50 -0400

    add tabs

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,52 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, React } from 'react'
 import { useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 function Details() {
     const params = useParams();
-    const [recipe, setRecipe] = useState({})
+    const [recipe, setRecipe] = useState({});
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
     useEffect(() => {
         getRecipe();
     }, [params.id]);
@@ -18,6 +61,18 @@ function Details() {
         <div>
             <h3>{recipe.title}</h3>
             <img src={recipe.image} />
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                    <Tab label="Instructions" {...a11yProps(0)} />
+                    <Tab label="Ingredients" {...a11yProps(1)} />
+                </Tabs>
+            </Box>
+            <TabPanel value={value} index={0}>
+                Item One
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item Two
+            </TabPanel>
         </div>
     )
 }
